904bb14bae7e4e812bbe91e0dd5bcbf735278ad4
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-10-10 08:07:53 +0300

    rec

--- rec.py
+++ rec.py
@@ -9,7 +9,7 @@ model = models[0].name
 
 # Define a prompt for medical-related questions
 exposure_percent=40
-weight=50
+weight=150
 height=1.70
 history_of_stroke="yes "
 family_history_of_stroke="yes"
@@ -18,14 +18,11 @@ diet="balaced "
 systolic_blood_pressure=50
 diastolic_blood_pressure=60
 
-
 medical_prompt = """
 "As an expert in stroke disease prevention, you play a crucial role in advising and developing
  personalized diet and exercise plans for patients based on their unique profiles. Your insights 
  are backed by extensive data analysis and a powerful model that calculates the risk of stroke.
-
 User Profile:
-
 Weight: {weight} kg
 Height: {height} meters
 Risk of a Stroke: {exposure_percent}%
@@ -36,10 +33,14 @@ Diet: {diet}
 Systolic Blood Pressure: {systolic_blood_pressure} mmHg
 Diastolic Blood Pressure: {diastolic_blood_pressure} mmHg
 It's essential to emphasize that the risk of stroke provided is based on meticulous data analysis and should be taken seriously.
+
 Taking into account the user's profile, it's commendable to acknowledge the positive aspects but also address areas that require improvement.
+
 Please provide a comprehensive set of recommendations, highlighting the favorable aspects of the user's profile while pinpointing areas that necessitate change.
-Please offer comprehensive recommendations tailored to the user's profile. Include specific guidance on lifestyle changes,
-dietary adjustments, and exercise routines that will not only promote better overall health but also reduce the risk of stroke effectively."
+
+Please offer comprehensive recommendations tailored to the user's profile. 
+
+Include specific guidance on lifestyle changes,dietary adjustments, and exercise routines that will not only promote better overall health but also reduce the risk of stroke effectively."
 """
 medical_response = palm.generate_text(
     model=model,
